2cf4bb458191d7a3d3b083bc9c5a9c3eca0a39b7
Gustavo Souza <gustavoelia7@gmail.com>
2023-12-17 14:03:32 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Photo Gallery</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" /> <!-- fancybox -->
+    <link href="assets/css/style.css" rel="stylesheet" /> <!-- main css file -->
+</head>
+<body>
+
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script> <!-- jQuery API -->
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script> <!-- fancybox -->
+    <script src="assets/js/script.js"></script> <!-- main script file -->
+</body>
+</html>
